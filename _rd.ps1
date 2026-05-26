c2d301fe08ff0db753f9f2c14100fc281d072d8b
Nikita <stithc92@gmail.com>
2026-05-26 21:21:56 +0300

    fix_project_group_flat_php_proxies

--- _rd.ps1
+++
@@ -1,5 +0,0 @@
-$lines = Get-Content 'C:\Users\user\Desktop\weather\mobile_app\lib\features\home\home_page.dart'
-for ($i = 3820; $i -lt 3900; $i++) {
-    $lineNum = $i + 1
-    Write-Host "$lineNum : $($lines[$i])"
-}
---
+++ backend_api/public/family_groups_create.php
@@ -0,0 +1,6 @@
+<?php
+
+declare(strict_types=1);
+
+require __DIR__ . '/_route.php';
+dispatch_flat_route('POST', '/family-groups/create');
